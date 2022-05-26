f492d997a1860e47325a7c8d5c74157ac8e4cfeb
haidy <96634979+hydee2003@users.noreply.github.com>
2022-05-26 21:01:12 +0200

    Add files via upload

---
+++ java.js
@@ -0,0 +1,65 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const error = document.querySelector('#error');
+var count = 0;
+
+//Show input error message
+function ShowError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'input-box-error';
+  //   const small = formControl.querySelector('small');
+  error.innerText += message + '\n';
+  count++;
+}
+
+//Show input success
+function ShowSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'input-box-success';
+}
+
+function CheckRequired(inputErr) {
+  inputErr.forEach(function (input) {
+    if (input.value.trim() === '') {
+      ShowError(input, `${getFieldName(input)} is required`);
+    } else {
+      ShowSuccess(input);
+    }
+  });
+}
+
+function CheckPassword(input) {
+  if (password.value.length !== 8) {
+    ShowError(input, `${getFieldName(input)} must be 8 characters no more no less`);
+  }
+  if (/[a-zA-Z]/.test(password.value.substring(0, 1)) != true) {
+    ShowError(input, `${getFieldName(input)}  must start with uppercase character`);
+  }
+  if (password.value.substring(0, 1) !== password.value.substring(0, 1).toUpperCase()) {
+    ShowError(input, `${getFieldName(input)}  must start with Upper case letter`);
+  }
+  if (/\d/.test(password.value) != true) {
+    ShowError(input, `${getFieldName(input)}  must include at least one number`);
+  }
+  if (/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(password.value) != true) {
+    ShowError(input, `${getFieldName(input)}  must include at least one special charactar`);
+  }
+  if (/\s/.test(password.value) == true) {
+    ShowError(input, `${getFieldName(input)}  can not contain white spaces`);
+  }
+}
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+form.addEventListener('submit', (e) => {
+  count = 0;
+  error.innerText = '';
+  CheckRequired([password]);
+  CheckPassword(password);
+  if (count !== 0) {
+    e.preventDefault();
+  }
+});
